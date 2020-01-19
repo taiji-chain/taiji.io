714d351926b4ce29fa362a3f7bcfab4d8d637123
Steve Hu <stevehu@gmail.com>
2020-01-19 10:35:36 -0500

    fixes #1 update generated App.js to add repository link

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The site is under contruction and the source code is located at
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/taiji-chain/taiji.io"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Source Code Repository
         </a>
       </header>
     </div>
